a59a371a5894d93f68a832e016dd90a566fddc51
nearbycoder <nearbycoder@gmail.com>
2016-04-17 11:17:00 -0500

    Fix issue with controller not having $onInit function

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -2,9 +2,6 @@ export default class HomeController {
   constructor (githubService) {
     'ngInject';
     this.githubService = githubService;
-  }
-
-  $onInit () {
     this.repos = this.githubService.getRepos({username: 'nearbycoder'});
   }
 }
